f403540ecd3fa875f3c8a3471dfd9db3e58052e4
Julianne Vela <75726146+julianne-vela@users.noreply.github.com>
2021-05-12 14:03:37 -0700

    okay testing is good for real now

--- src/components/villagers/VillagerDetail.jsx
+++ src/components/villagers/VillagerDetail.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-function VillagerDetail({
-  name,
-  japaneseName,
-  image,
-  species,
-  birthday,
-  favSong,
-  quote,
-  personality,
-  style,
-}) {
+function VillagerDetail({ villager }) {
   return (
     <main role="villagerDetails">
-      <label>{name}</label>
-      <label>{japaneseName}</label>
-      <img src={image} alt={name} />
+      <label>{villager.name}</label>
+      <label>{villager.japaneseName}</label>
+      <img src={villager.image} alt={villager.name} />
       <ul>
-        <li>{species}</li>
-        <li>{birthday}</li>
-        <li>{favSong}</li>
-        <li>{quote}</li>
-        <li>{personality}</li>
-        <li>{style}</li>
+        <li>{villager.species}</li>
+        <li>{villager.birthday}</li>
+        <li>{villager.favSong}</li>
+        <li>{villager.quote}</li>
+        <li>{villager.personality}</li>
+        <li>{villager.style}</li>
       </ul>
       <a href="/">Back to List</a>
     </main>
   );
 }
 
-VillagerDetail.propTypes = {};
+// VillagerDetail.propTypes = {
+//   birthday: PropTypes.string.isRequired,
+//   favSong: PropTypes.string.isRequired,
+//   image: PropTypes.string.isRequired,
+//   japaneseName: PropTypes.string.isRequired,
+//   name: PropTypes.string.isRequired,
+//   personality: PropTypes.string.isRequired,
+//   quote: PropTypes.string.isRequired,
+//   species: PropTypes.string.isRequired,
+//   style: PropTypes.string.isRequired,
+// };
 
 export default VillagerDetail;
